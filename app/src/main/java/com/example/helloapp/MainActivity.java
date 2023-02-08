59c73c077204c173ab5456f0a3696acaa47d21b1
dannnik4 <daniilperstnev2001@gmail.com>
2023-02-08 22:08:38 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -10,14 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // создание TextView
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
         TextView textView = new TextView(this);
-        // установка текста в TextView
         textView.setText("Hello Android!");
-        // установка высоты текста
-        textView.setTextSize(22);
-        // установка визуального интерфейса для activity
-        setContentView(textView);
+        textView.setTextSize(26);
+        // устанавливаем параметры размеров и расположение элемента
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        // выравнивание по левому краю ConstraintLayout
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        // выравнивание по верхней границе ConstraintLayout
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        // устанавливаем параметры для textView
+        textView.setLayoutParams(layoutParams);
+        // добавляем TextView в ConstraintLayout
+        constraintLayout.addView(textView);
+        // в качестве корневого
+        setContentView(constraintLayout);
 
     }
 }
