900907ae062da3ee8deee3961f1f056bee97b248
dannnik4 <daniilperstnev2001@gmail.com>
2023-02-08 22:00:08 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // создание TextView
+        TextView textView = new TextView(this);
+        // установка текста в TextView
+        textView.setText("Hello Android!");
+        // установка высоты текста
+        textView.setTextSize(22);
+        // установка визуального интерфейса для activity
+        setContentView(textView);
+
     }
 }
