0aa21557042a0ced618ce0d5a4fc4af0d6d8b301
Daniil Perstnov <95372410+dannnik4@users.noreply.github.com>
2023-02-10 13:53:32 +0200

    Revert "View app"

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -2,20 +2,12 @@ package com.example.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
-
-        // получаем элемент textView
-        TextView textView = findViewById(R.id.header);
-        // переустанавливаем у него текст
-        textView.setText("Hello from Java!");
-
+        setContentView(R.layout.activity_main);
     }
-
 }
