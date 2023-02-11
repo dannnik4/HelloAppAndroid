ffe72d1d3b650db2a01d1679392837174d27360e
dannnik4 <daniilperstnev2001@gmail.com>
2023-02-11 12:25:42 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.third_layout);
+        setContentView(R.layout.fourth_layout);
 
         // создание TextView
         //TextView textView = new TextView(this);
