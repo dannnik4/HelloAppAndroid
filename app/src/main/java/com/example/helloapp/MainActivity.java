11449318301c2edd4114a7b012142d4491d8ffd9
dannnik4 <daniilperstnev2001@gmail.com>
2023-02-18 22:58:23 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -128,6 +129,37 @@ public class MainActivity extends AppCompatActivity {
         //textView1.setTextSize(30);
         //linearLayout.addView(textView1, layoutParams);
         //setContentView(linearLayout);
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+
+        EditText editText = new EditText(this);
+        editText.setId(EditText.generateViewId());
+
+        Button button = new Button(this);
+        button.setText("Отправить");
+
+        // устанавливаем параметры положения для EditText
+        RelativeLayout.LayoutParams editTextParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        // выравнивание по центру родительского контейнера
+        editTextParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        // добавляем в RelativeLayout
+        relativeLayout.addView(editText, editTextParams);
+
+        // устанавливаем параметры положения для Button
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        // выравнивание справа и снизу от поля EditText
+        buttonParams.addRule(RelativeLayout.BELOW, editText.getId());
+        buttonParams.addRule(RelativeLayout.ALIGN_RIGHT, editText.getId());
+        // добавляем в RelativeLayout
+        relativeLayout.addView(button, buttonParams);
+
+        setContentView(relativeLayout);
 
     }
 }
