7ba2c114fa78e284ead3a4ce4032f22b191dee3a
dannnik4 <daniilperstnev2001@gmail.com>
2023-02-24 21:05:11 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -303,5 +303,28 @@ public class MainActivity extends AppCompatActivity {
         //    clicks--;
         //    clicksText.setText(clicks + " Clicks");
         //});
+
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        TextView textView = new TextView(this);
+        textView.setText("Hello Android!");
+        textView.setTextSize(30);
+        textView.setBackgroundColor(0xffe8eaf6);
+
+        // установка gravity
+        textView.setGravity(Gravity.CENTER);
+
+        // установка высоты и ширины
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, 200);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
+        textView.setLayoutParams(layoutParams);
+
+        constraintLayout.addView(textView);
+        setContentView(constraintLayout);
+
     }
 }
