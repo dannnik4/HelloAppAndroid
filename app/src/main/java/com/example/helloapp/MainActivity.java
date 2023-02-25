8478ffca907665d2e1e5c99f4a9e5bc1079c6227
dannnik4 <daniilperstnev2001@gmail.com>
2023-02-25 22:47:04 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.view.ViewGroup;
 import android.widget.ScrollView;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -325,6 +326,33 @@ public class MainActivity extends AppCompatActivity {
 
         //constraintLayout.addView(textView);
         //setContentView(constraintLayout);
+
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        TextView textView = new TextView(this);
+        // установка фонового цвета
+        textView.setBackgroundColor(0xffe8eaf6);
+        // установка цвета текста
+        textView.setTextColor(0xff5c6bc0);
+        // делаем все буквы заглавными
+        textView.setAllCaps(true);
+        // устанавливаем вравнивание текста по центру
+        textView.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        // устанавливаем текста
+        textView.setText("Hello Android!");
+        // установка шрифта
+        textView.setTypeface(Typeface.create("casual", Typeface.NORMAL));
+        // устанавливаем высоту текста
+        textView.setTextSize(26);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        textView.setLayoutParams(layoutParams);
+
+        constraintLayout.addView(textView);
+        setContentView(constraintLayout);
 
     }
 }
