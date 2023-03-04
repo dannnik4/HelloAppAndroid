cb51a99e6aa20c0faed968cadb0daf99aa8578c3
dannnik4 <daniilperstnev2001@gmail.com>
2023-03-04 23:03:35 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -541,18 +541,42 @@ public class MainActivity extends AppCompatActivity {
         //        }
         //});
 
-    }
+        //}
 
-    public void onToggleClicked(View view) {
+        //public void onToggleClicked(View view) {
 
         // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            // действия если включена
-            Toast.makeText(this, "Свет включен", Toast.LENGTH_LONG).show();
-        } else {
-            // действия, если выключена
-            Toast.makeText(this, "Свет выключен!", Toast.LENGTH_LONG).show();
-        }
+        //    boolean on = ((ToggleButton) view).isChecked();
+        //    if (on) {
+        //        // действия если включена
+        //        Toast.makeText(this, "Свет включен", Toast.LENGTH_LONG).show();
+        //    } else {
+        //        // действия, если выключена
+        //        Toast.makeText(this, "Свет выключен!", Toast.LENGTH_LONG).show();
+        //    }
+
+        ConstraintLayout layout = new ConstraintLayout(this);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        ToggleButton toggleButton = new ToggleButton(this);
+        toggleButton.setTextOff("Выключено");
+        toggleButton.setTextOn("Включено");
+        toggleButton.setText("Выключено");
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean on = ((ToggleButton) view).isChecked();
+
+                if (on) {
+                    Toast.makeText(getApplicationContext(), "Свет включен", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Свет выключен!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.addView(toggleButton);
+        setContentView(layout);
     }
 }
