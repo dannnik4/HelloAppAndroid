e9d784edc6f9612719732e4ddce2ff4310d20ea6
dannnik4 <daniilperstnev2001@gmail.com>
2023-03-12 20:38:58 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dimen_layout);
+        setContentView(R.layout.colors_layout);
 
         // создание TextView
         //TextView textView = new TextView(this);
@@ -773,31 +773,34 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(textView);
 
 
-        // получаем ресурсы
-        Resources resources = getResources();
-        float textSize = resources.getDimension(R.dimen.text_size);
-        int hMargin = (int)resources.getDimension(R.dimen.horizontal_margin);
-        int vMargin = (int)resources.getDimension(R.dimen.vertical_margin);
+//        // получаем ресурсы
+//        Resources resources = getResources();
+//        float textSize = resources.getDimension(R.dimen.text_size);
+//        int hMargin = (int)resources.getDimension(R.dimen.horizontal_margin);
+//        int vMargin = (int)resources.getDimension(R.dimen.vertical_margin);
+//
+//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+//
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+//                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+//
+//        TextView textView = new TextView(this);
+//        textView.setText("Hello Android");
+//        textView.setBackgroundColor(0XFFEAEAEA);
+//        // устанавливаем размер шрифт по ресурсу
+//        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+//        // устанавливаем отступы пв соответствии с ресурсами
+//        layoutParams.setMargins(hMargin, vMargin, hMargin, vMargin);
+//
+//        textView.setLayoutParams(layoutParams);
+//        constraintLayout.addView(textView);
+//
+//        setContentView(constraintLayout);
 
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
 
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
-                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
 
-        TextView textView = new TextView(this);
-        textView.setText("Hello Android");
-        textView.setBackgroundColor(0XFFEAEAEA);
-        // устанавливаем размер шрифт по ресурсу
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-        // устанавливаем отступы пв соответствии с ресурсами
-        layoutParams.setMargins(hMargin, vMargin, hMargin, vMargin);
-
-        textView.setLayoutParams(layoutParams);
-        constraintLayout.addView(textView);
-
-        setContentView(constraintLayout);
 
     }
 }
