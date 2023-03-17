725bb0f3b0e32a07908fd483de7baaf1d0031893
dannnik4 <daniilperstnev2001@gmail.com>
2023-03-17 20:25:36 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intent_layout);
+        setContentView(R.layout.parcelable_layout);
 
         // создание TextView
         //TextView textView = new TextView(this);
@@ -878,6 +878,30 @@ public class MainActivity extends AppCompatActivity {
 //        startActivity(intent);
 
 
+//    }
+//    public void onClick(View v) {
+//
+//        EditText nameText = findViewById(R.id.name);
+//        EditText companyText = findViewById(R.id.company);
+//        EditText ageText = findViewById(R.id.age);
+//
+//        String name = nameText.getText().toString();
+//        String company = companyText.getText().toString();
+//        int age = Integer.parseInt(ageText.getText().toString());
+//
+//        //Intent intent = new Intent(this, SecondActivity.class);
+//        //intent.putExtra("name", name);
+//        //intent.putExtra("company", company);
+//        //intent.putExtra("age", age);
+//        //startActivity(intent);
+//
+//        User user = new User(name, company, age);
+//
+//        Intent intent = new Intent(this, SecondActivity.class);
+//        intent.putExtra(User.class.getSimpleName(), user);
+//        startActivity(intent);
+
+
     }
     public void onClick(View v) {
 
@@ -888,12 +912,6 @@ public class MainActivity extends AppCompatActivity {
         String name = nameText.getText().toString();
         String company = companyText.getText().toString();
         int age = Integer.parseInt(ageText.getText().toString());
-
-        //Intent intent = new Intent(this, SecondActivity.class);
-        //intent.putExtra("name", name);
-        //intent.putExtra("company", company);
-        //intent.putExtra("age", age);
-        //startActivity(intent);
 
         User user = new User(name, company, age);
 
