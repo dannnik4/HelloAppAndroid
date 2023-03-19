8a742d6040cd705171f44986226a20bdda2f9804
Daniil Perstnov <95372410+dannnik4@users.noreply.github.com>
2023-03-19 22:17:34 +0200

    Revert "View app"

--- app/src/main/java/com/example/helloapp/SecondActivity.java
+++ app/src/main/java/com/example/helloapp/SecondActivity.java
@@ -2,9 +2,7 @@ package com.example.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -12,7 +10,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.result_second);
+//        setContentView(R.layout.activity_second);
 
 //        TextView textView = new TextView(this);
 //        textView.setTextSize(20);
@@ -45,48 +43,19 @@ public class SecondActivity extends AppCompatActivity {
 //        setContentView(textView);
 
 
-//        TextView textView = new TextView(this);
-//        textView.setTextSize(26);
-//        textView.setPadding(16, 16, 16, 16);
-//
-//        Bundle arguments = getIntent().getExtras();
-//
-//        User user;
-//        if(arguments!=null){
-//            user = arguments.getParcelable(User.class.getSimpleName());
-//
-//            textView.setText("Name: " + user.getName() + "\nCompany: " + user.getCompany() +
-//                    "\nAge: " + String.valueOf(user.getAge()));
-//        }
-//        setContentView(textView);
+        TextView textView = new TextView(this);
+        textView.setTextSize(26);
+        textView.setPadding(16, 16, 16, 16);
 
+        Bundle arguments = getIntent().getExtras();
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            TextView ageView = findViewById(R.id.ageView);
-            String age = extras.getString(MainActivity.AGE_KEY);
-            ageView.setText("Возраст: " +  age);
+        User user;
+        if(arguments!=null){
+            user = arguments.getParcelable(User.class.getSimpleName());
+
+            textView.setText("Name: " + user.getName() + "\nCompany: " + user.getCompany() +
+                    "\nAge: " + String.valueOf(user.getAge()));
         }
-    }
-    public void onCancelClick(View v) {
-        setResult(RESULT_CANCELED);
-        finish();
-    }
-    public void onButton1Click(View v) {
-        sendMessage("Доступ разрешен");
-    }
-    public void onButton2Click(View v) {
-        sendMessage("Доступ запрещен");
-    }
-    public void onButton3Click(View v) {
-        sendMessage("Недопустимый возраст");
-    }
-    private void sendMessage(String message){
-
-        Intent data = new Intent();
-        data.putExtra(MainActivity.ACCESS_MESSAGE, message);
-        setResult(RESULT_OK, data);
-        finish();
-
+        setContentView(textView);
     }
 }
