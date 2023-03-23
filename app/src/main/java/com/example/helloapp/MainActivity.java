c069b849b37351fac64b51fbd7bd1dcde3192a8b
dannnik4 <daniilperstnev2001@gmail.com>
2023-03-23 15:05:16 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // набор данных, которые свяжем со списком
+    String[] countries = {"Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listview_layout);
+
+        // получаем элемент ListView
+        ListView countriesList = findViewById(R.id.countriesList);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, countries);
+
+        // устанавливаем для списка адаптер
+        countriesList.setAdapter(adapter);
+
     }
 }
