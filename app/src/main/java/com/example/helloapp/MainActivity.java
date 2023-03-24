b602d3dc545127dacd0d27fffd0cbff6bcb1083c
dannnik4 <daniilperstnev2001@gmail.com>
2023-03-24 21:00:09 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -8,16 +8,30 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // набор данных, которые свяжем со списком
-    String[] countries = {"Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
+//    // набор данных, которые свяжем со списком
+//    String[] countries = {"Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.listview_layout);
+        setContentView(R.layout.stringarray_layout);
+
+//        // получаем элемент ListView
+//        ListView countriesList = findViewById(R.id.countriesList);
+//
+//        // создаем адаптер
+//        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+//                android.R.layout.simple_list_item_1, countries);
+//
+//        // устанавливаем для списка адаптер
+//        countriesList.setAdapter(adapter);
+
 
         // получаем элемент ListView
         ListView countriesList = findViewById(R.id.countriesList);
+
+        // получаем ресурс
+        String[] countries = getResources().getStringArray(R.array.countries);
 
         // создаем адаптер
         ArrayAdapter<String> adapter = new ArrayAdapter(this,
