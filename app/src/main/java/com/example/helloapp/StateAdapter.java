954bab539ad74e7525e9b30e03c1b05c625b760e
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-03 23:03:01 +0300

    Update StateAdapter.java

--- app/src/main/java/com/example/helloapp/StateAdapter.java
+++ app/src/main/java/com/example/helloapp/StateAdapter.java
@@ -4,48 +4,62 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class StateAdapter extends ArrayAdapter<State> {
+public class StateAdapter  extends RecyclerView.Adapter<StateAdapter.ViewHolder>{
 
-    private LayoutInflater inflater;
-    private int layout;
-    private List<State> states;
+    interface OnStateClickListener{
+        void onStateClick(State state, int position);
+    }
 
-    public StateAdapter(Context context, int resource, List<State> states) {
-        super(context, resource, states);
+    private final OnStateClickListener onClickListener;
+
+    private final LayoutInflater inflater;
+    private final List<State> states;
+
+    StateAdapter(Context context, List<State> states, OnStateClickListener onClickListener) {
+        this.onClickListener = onClickListener;
         this.states = states;
-        this.layout = resource;
         this.inflater = LayoutInflater.from(context);
     }
-    public View getView(int position, View convertView, ViewGroup parent) {
+    @Override
+    public StateAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        ViewHolder viewHolder;
-        if(convertView==null){
-            convertView = inflater.inflate(this.layout, parent, false);
-            viewHolder = new ViewHolder(convertView);
-            convertView.setTag(viewHolder);
-        }
-        else{
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-        State state = states.get(position);
-
-        viewHolder.imageView.setImageResource(state.getFlagResource());
-        viewHolder.nameView.setText(state.getName());
-        viewHolder.capitalView.setText(state.getCapital());
-
-        return convertView;
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
     }
-    private class ViewHolder {
-        final ImageView imageView;
+
+    @Override
+    public void onBindViewHolder(StateAdapter.ViewHolder holder, int position) {
+        State state = states.get(position);
+        holder.flagView.setImageResource(state.getFlagResource());
+        holder.nameView.setText(state.getName());
+        holder.capitalView.setText(state.getCapital());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                onClickListener.onStateClick(state, position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return states.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView flagView;
         final TextView nameView, capitalView;
         ViewHolder(View view){
-            imageView = view.findViewById(R.id.flag);
+            super(view);
+            flagView = view.findViewById(R.id.flag);
             nameView = view.findViewById(R.id.name);
             capitalView = view.findViewById(R.id.capital);
         }
