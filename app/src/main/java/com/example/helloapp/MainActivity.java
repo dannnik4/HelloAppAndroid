78cdd11d9480dd3bccf2144bb536e2fd7b17cf36
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-11 19:57:00 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.helloapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_layout);
     }
 
     @Override
@@ -17,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = findViewById(R.id.selectedMenuItem);
+        switch(id){
+            case R.id.action_settings :
+                headerView.setText("Настройки");
+                return true;
+            case R.id.open_settings:
+                headerView.setText("Открыть");
+                return true;
+            case R.id.save_settings:
+                headerView.setText("Сохранить");
+                return true;
+        }
+        //headerView.setText(item.getTitle());
+        return super.onOptionsItemSelected(item);
     }
 }
