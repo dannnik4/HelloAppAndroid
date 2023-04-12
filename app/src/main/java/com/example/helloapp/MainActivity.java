0e6eec263213c3e8bdc775853a02e633d4177e1d
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-12 23:45:25 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_layout);
+        setContentView(R.layout.activity_main);
     }
 
 //    @Override
@@ -40,21 +40,47 @@ public class MainActivity extends AppCompatActivity {
 //        return super.onOptionsItemSelected(item);
 //    }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//        menu.add("Настройки");
+//        menu.add("Открыть");
+//        menu.add("Сохранить");
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        String title = item.getTitle().toString();
+//        TextView headerView = findViewById(R.id.selectedMenuItem);
+//        headerView.setText(title);
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add("Настройки");
-        menu.add("Открыть");
-        menu.add("Сохранить");
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        String title = item.getTitle().toString();
+        int id = item.getItemId();
         TextView headerView = findViewById(R.id.selectedMenuItem);
-        headerView.setText(title);
-
+        switch(id){
+            case R.id.action_settings :
+                headerView.setText("Настройки");
+                return true;
+            case R.id.open_settings:
+                headerView.setText("Открыть");
+                return true;
+            case R.id.save_settings:
+                headerView.setText("Сохранить");
+                return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+
 }
