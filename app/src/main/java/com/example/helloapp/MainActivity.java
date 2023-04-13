fdf80e605991172f6ea7f69f49a3a5bcc5f05892
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-13 11:08:19 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_view, ContentFragment.class, null)
+                    .commit();
+        }
+
     }
 }
