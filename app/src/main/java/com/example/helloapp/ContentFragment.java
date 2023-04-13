e5ecec7904839897dd158929d77fcdb047a0ec71
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-13 10:54:47 +0300

    Add ContentFragment.xml

---
+++ app/src/main/java/com/example/helloapp/ContentFragment.java
@@ -0,0 +1,10 @@
+package com.example.helloapp;
+
+import androidx.fragment.app.Fragment;
+
+public class ContentFragment extends Fragment {
+
+    public ContentFragment(){
+        super(R.layout.fragment_content);
+    }
+}
