e1261b652bd723aedc79277f4522ecb5b8e907d4
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-30 19:32:09 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -10,23 +10,41 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     String name ="undefined";
-
+    final static String nameVariableKey = "NAME_VARIABLE";
+    TextView nameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instance_layout);
+
+        nameView = findViewById(R.id.nameView);
     }
 
+    // сохранение состояния
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        outState.putString(nameVariableKey, name);
+        super.onSaveInstanceState(outState);
+    }
+    // получение ранее сохраненного состояния
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        name = savedInstanceState.getString(nameVariableKey);
+        nameView.setText(name);
+    }
     public void saveName(View view) {
 
         // получаем введенное имя
         EditText nameBox = findViewById(R.id.nameBox);
+        //  сохраняем его в переменную name
         name = nameBox.getText().toString();
     }
     public void getName(View view) {
 
-        // получаем сохраненное имя
-        TextView nameView = findViewById(R.id.nameView);
+        //  выводим сохраненное имя
         nameView.setText(name);
     }
 }
