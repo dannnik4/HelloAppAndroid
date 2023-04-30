c4e9f2f6c6dcb70b001c957677766b2c8860f82e
dannnik4 <daniilperstnev2001@gmail.com>
2023-04-30 19:31:54 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String name ="undefined";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.instance_layout);
+    }
+
+    public void saveName(View view) {
+
+        // получаем введенное имя
+        EditText nameBox = findViewById(R.id.nameBox);
+        name = nameBox.getText().toString();
+    }
+    public void getName(View view) {
+
+        // получаем сохраненное имя
+        TextView nameView = findViewById(R.id.nameView);
+        nameView.setText(name);
     }
 }
