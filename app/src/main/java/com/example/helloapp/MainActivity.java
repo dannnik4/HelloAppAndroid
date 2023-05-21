8eed61d123ff7db1a5401f0f283fbd62df776ba3
dannnik4 <daniilperstnev2001@gmail.com>
2023-05-21 12:51:54 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -1556,25 +1556,70 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
 
-    userList = findViewById(R.id.list);
-        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent intent = new Intent(getApplicationContext(), UserActivity.class);
-            intent.putExtra("id", id);
-            startActivity(intent);
-        }
-    });
+//    userList = findViewById(R.id.list);
+//        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+//            intent.putExtra("id", id);
+//            startActivity(intent);
+//        }
+//    });
+//
+//    databaseHelper = new DatabaseHelper(getApplicationContext());
+//}
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        // открываем подключение
+//        db = databaseHelper.getReadableDatabase();
+//
+//        //получаем данные из бд в виде курсора
+//        userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
+//        // определяем, какие столбцы из курсора будут выводиться в ListView
+//        String[] headers = new String[]{DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+//        // создаем адаптер, передаем в него курсор
+//        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+//                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+//        userList.setAdapter(userAdapter);
+//    }
+//
+//    // по нажатию на кнопку запускаем UserActivity для добавления данных
+//    public void add(View view) {
+//        Intent intent = new Intent(this, UserActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        // Закрываем подключение и курсор
+//        db.close();
+//        userCursor.close();
+//    }
 
-    databaseHelper = new DatabaseHelper(getApplicationContext());
-}
+
+        userList = findViewById(R.id.list);
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+        // создаем базу данных
+        databaseHelper.create_db();
+    }
 
     @Override
     public void onResume() {
         super.onResume();
         // открываем подключение
-        db = databaseHelper.getReadableDatabase();
-
+        db = databaseHelper.open();
         //получаем данные из бд в виде курсора
         userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
         // определяем, какие столбцы из курсора будут выводиться в ListView
