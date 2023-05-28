d57029e698bfcd7ca9a72649a254511e67b7b543
dannnik4 <daniilperstnev2001@gmail.com>
2023-05-28 15:24:19 +0300

    Update CustomDialogFragment.java

--- app/src/main/java/com/example/helloapp/CustomDialogFragment.java
+++ app/src/main/java/com/example/helloapp/CustomDialogFragment.java
@@ -11,7 +11,17 @@ public class CustomDialogFragment extends DialogFragment {
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-        return builder.setTitle("Диалоговое окно").setMessage("Для закрытия окна нажмите ОК").create();
+//        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+//        return builder.setTitle("Диалоговое окно").setMessage("Для закрытия окна нажмите ОК").create();
+//    }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("Диалоговое окно")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setMessage("Для закрытия окна нажмите ОК")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Отмена", null)
+                .create();
     }
 }
